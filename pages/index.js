63163b844ccd841dbb4c925b17bc1d3b8dc13266
burnworks <yoshiki@burnworks.com>
2020-12-15 19:04:32 +0900

    Yup のアップデートとそれに伴うコード変更

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,8 @@ const contactForm = () => {
                         inquiryType: Yup.string()
                             .required('お問い合わせ種別を選択してください'),
                         service: Yup.array()
-                            .required('検討中のサービスを1つ以上選択してください'),
+                            .required()
+                            .min(1, '検討中のサービスを1つ以上選択してください'),
                         name: Yup.string()
                             .required('ご担当者名は必須です'),
                         company: Yup.string()
